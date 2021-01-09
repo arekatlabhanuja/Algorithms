76f04677421278d568d223d8cc7c7e8499ec7ab5
arekatlabhanuja <36080380+arekatlabhanuja@users.noreply.github.com>
2021-01-09 13:31:44 +0530

    Add files via upload

---
+++ Sorting Algorithms/Bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr):
+   
+    for i in range(0,len(arr)):
+         
+
+         for j in range(i+1,len(arr)):
+             if(arr[i]>arr[j]):
+                 arr[i],arr[j]=arr[j],arr[i]
+                 
+    return arr
+
+arr = list(map(int, input().split()))
+print(bubble_sort(arr))
